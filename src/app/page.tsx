e247c9e407b799ea5af10b9b07ce68ab8e2deceb
Lucas <lopesgabrielpc@gmail.com>
2025-06-03 16:45:34 -0400

    Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+
+import { Suspense } from "react";
 import LoginForm from "../components/LoginForm";
 
 export default function Home() {
   return (
     <main>
       <div className="h-screen flex justify-center items-center bg-slate-600 px-5">
+      <Suspense fallback={<div>Carregando...</div>}>
         <LoginForm />
+      </Suspense>
       </div>
     </main>
   );
